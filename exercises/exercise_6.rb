cce0cf0f81cf26156751cc061456f8af327cc9b9
Adam Underhill <adameunderhill@gmail.com>
2021-07-26 18:22:47 -0700

    adding validations for both stores and employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -11,8 +11,8 @@ puts "----------"
 # Your code goes here ...
 
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
-@store2.employees.create(first_name: "a", last_name: "b", hourly_rate: 35)
-@store1.employees.create(first_name: "c", last_name: "d", hourly_rate: 40)
-@store1.employees.create(first_name: "e", last_name: "f", hourly_rate: 45)
-@store1.employees.create(first_name: "j", last_name: "k", hourly_rate: 55)
-@store2.employees.create(first_name: "l", last_name: "m", hourly_rate: 33)
+@store2.employees.create(first_name: "a", last_name: "b", hourly_rate: 100)
+@store1.employees.create(first_name: "c", last_name: "d", hourly_rate: 110)
+@store1.employees.create(first_name: "e", last_name: "f", hourly_rate: 120)
+@store1.employees.create(first_name: "j", last_name: "k", hourly_rate: 140)
+@store2.employees.create(first_name: "l", last_name: "m", hourly_rate: 150)
